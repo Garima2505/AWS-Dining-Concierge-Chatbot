45f5bdbf0f89627a4d17b397919f3137b0091895
Garima Chaudhary <gc2505@nyu.edu>
2020-10-23 16:37:21 -0400

    add Lambda function

---
+++ Lf0.py
@@ -0,0 +1,35 @@
+import json
+import datetime
+import boto3
+import os
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
+def lambda_handler(event, context):
+    message = event['messages']
+    bot_response_message = "Please Try again!"
+
+    if message is not None or len(message) > 0:
+        data = message[0]['unstructured']['text']
+        client = boto3.client('lex-runtime')
+        bot_response = client.post_text(botName='diningBot', botAlias='foodoBot', userId='rahu', inputText=data)
+
+        bot_response_message = bot_response['message']
+
+    response = {
+        'messages': [
+            {
+                "type": "unstructured",
+                "unstructured": {
+                    "id": "1",
+                    "text": bot_response_message,
+                    "timestamp": str(datetime.datetime.now().timestamp())
+                }
+            }
+        ]
+    }
+
+    return response
